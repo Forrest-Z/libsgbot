7c9418c458e828e8cecc029a3270bbef7f10268a
cyberniklee <cyberniklee@gmail.com>
2018-02-23 22:12:06 -0800

    first commit codes

--- build/src/CMakeFiles/sgbot.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libsgbot.pdb"
-  "libsgbot.so"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/sgbot.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/src/std-math/CMakeFiles/std-math.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libstd-math.a"
-)
