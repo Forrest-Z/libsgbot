d1d3ff3e257cd3adfe8a76cbd93d9527e0632f58
cyberniklee <cyberniklee@gmail.com>
2018-04-03 12:37:01 +0800

    add gitignore rules

---
+++ build/samples/CMakeFiles/tf-sample.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tf-sample.dir/tf-sample.cpp.o"
+  "tf-sample.pdb"
+  "tf-sample"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tf-sample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
